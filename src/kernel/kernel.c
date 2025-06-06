55199a7b3ea76dd1b9545b9427b194f2036e81e4
Flapjacck <plkelly333@me.com>
2025-06-06 18:40:53 -0400

    removed duplicate code on kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -13,30 +13,6 @@
 
 #include "kernel.h"
 
-/* Hardware text mode color constants */
-enum vga_color {
-    VGA_COLOR_BLACK = 0,
-    VGA_COLOR_BLUE = 1,
-    VGA_COLOR_GREEN = 2,
-    VGA_COLOR_CYAN = 3,
-    VGA_COLOR_RED = 4,
-    VGA_COLOR_MAGENTA = 5,
-    VGA_COLOR_BROWN = 6,
-    VGA_COLOR_LIGHT_GREY = 7,
-    VGA_COLOR_DARK_GREY = 8,
-    VGA_COLOR_LIGHT_BLUE = 9,
-    VGA_COLOR_LIGHT_GREEN = 10,
-    VGA_COLOR_LIGHT_CYAN = 11,
-    VGA_COLOR_LIGHT_RED = 12,
-    VGA_COLOR_LIGHT_MAGENTA = 13,
-    VGA_COLOR_LIGHT_BROWN = 14,
-    VGA_COLOR_WHITE = 15,
-};
-
-/* Constants for VGA text mode buffer */
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
-
 /* Global variables for terminal state */
 size_t terminal_row;
 size_t terminal_column;
